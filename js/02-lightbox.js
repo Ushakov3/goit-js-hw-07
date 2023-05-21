f16984c86095e4fffc5695e6149669b94d2dfd87
Ushakov Aleksandr <122167617+Ushakov3@users.noreply.github.com>
2023-05-21 13:46:35 +0300

    t2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function createGalleryList(items) {
+  const markup = items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+  return markup;
+}
+
+const cardMarkup = createGalleryList(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", cardMarkup);
+
+const options = {
+  captionsData: "alt",
+  captionDelay: 250,
+};
+
+const lightbox = new SimpleLightbox(".gallery__item a", options);
